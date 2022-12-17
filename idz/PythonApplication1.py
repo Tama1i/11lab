ff25465b9b6f5f10fdcbaaf244eadcb2f0e96540
Tama1i <ryazantseff.matvei@yandex.ru>
2022-12-17 15:46:13 +0300

    FIX

--- idz/PythonApplication1.py
+++ idz/PythonApplication1.py
@@ -52,19 +52,17 @@ def li(pep):
         print(line)
 
 
-def sel(pep):
-     # Получить требуемый стаж.
-     zapros = int(input("zapros po numeru  "))
-
+def sel(pep,zapros):
             # Инициализировать счетчик.
      count = 0
             # Проверить сведения работников из списка.
      for chel in pep:
         if chel.get('num') == zapros:
             count += 1
-            print(
-                 '{:>4}: {}'.format(count, chel.get('name', ''))
-            )
+            return chel.get, count
+            #print(
+            #     '{:>4}: {}'.format(count, chel.get('name', ''))
+            #)
 
             # Если счетчик равен 0, то работники не найдены.
      if count == 0:
@@ -74,7 +72,7 @@ from datetime import date
 if __name__ == '__main__':
     # Список работников.
     pep = []
-
+    s = ""
     # Организовать бесконечный цикл запроса команд.
     while True:
         # Запросить команду из терминала.
@@ -90,7 +88,12 @@ if __name__ == '__main__':
         elif command == 'list':
             li(pep)
         elif command == 'select':
-            sel(pep)
+            zapros = int(input("zapros po numeru  "))
+            s, count = sel(pep,zapros)
+            print(
+                 '{:>4}: {}'.format(count, s('name', ''))
+            )
+            
         elif command == 'help':
             # Вывести справку о работе с программой.
             print("Список команд:\n")
@@ -101,4 +104,5 @@ if __name__ == '__main__':
             print("exit - завершить работу с программой.")
         else:
             print(f"Неизвестная команда {command}", file=sys.stderr)
+
 
